ef3a4a8e07f30515f02f9d0bb4cf40daeaa5f956
NUIG2017 <32104673+NUIG2017@users.noreply.github.com>
2017-09-25 11:04:50 +0100

    Update shinyAppTutorial.R

--- shinyAppTutorial.R
+++ shinyAppTutorial.R
@@ -50,7 +50,7 @@ server <- function(input, output) {
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
+    hist(x, breaks = bins, col = "blue", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
 
